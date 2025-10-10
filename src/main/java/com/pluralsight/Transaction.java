d08251541cf56940ed3f344f2b08116c3f7587e1
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-10 14:01:52 -0500

    added constructor and getters

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -7,10 +7,35 @@ public class Transaction {
     private String vendor;
     private double amount;
 
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = "";
+        this.time = "";
+        this.description = "";
+        this.vendor = "";
+        this.amount = 0;
+    }
 
+    public Transaction() {
 
+    }
 
+    public String getDate() {
+        return date;
+    }
 
+    public String getTime() {
+        return time;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
